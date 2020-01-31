910885773274ca8be154f76c5b194a21f296d015
uzmar.gomez@ciencias.unam.mx <uzmar.gomez@ciencias.unam.mx>
2020-01-30 20:48:50 -0600

    docker modified

--- tests/src/demo.cpp
+++ tests/src/demo.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    cv::Mat M;
-    std::cout<<"hola"<<std::endl;
+    cv::Mat C = (cv::Mat_<double>(3,3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+    std::cout<<"C = " << std::endl << " " << C << std::endl << std::endl;
     return 0;
 }
