3af2ab7040644c755271d59226717ea47ce40e5f
uzmar.gomez@ciencias.unam.mx <uzmar.gomez@ciencias.unam.mx>
2020-02-03 12:29:01 -0600

    Modify c++

--- tests/clanguage/src/demo.cpp
+++ tests/clanguage/src/demo.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <chrono>
+#include <vector>
 #include <opencv2/core.hpp>
+#include <opencv2/core/utility.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
@@ -13,10 +15,22 @@ int main()
     //cv::Mat A = (cv::Mat_<double>(4,3) << 3, 2, 2, 2, 3, -2, -5, 0, 1, 4, 6, -8);
     
     // Image 
-    cv::Mat A = cv::imread("amber.jpg",0);
+    cv::Mat A = cv::imread("images/amber.jpg",0);
     cv::Mat_<double> B;
 
-    A.convertTo(B,CV_64F);
+    std::vector< cv::String > fn;
+    std::vector< cv::Mat > im;
+    std::vector< cv::Mat_<double> > dat_im;
+
+    cv::glob("images/*.jpg",fn,true);
+    for (size_t k = 0; k < fn.size(); ++k){
+        im.push_back(cv::imread(fn[k],0));
+        dat_im.push_back(cv::imread(fn[k],0));
+    }
+
+    for (size_t k = 0; k < fn.size(); ++k){
+        im[k].convertTo(dat_im[k],CV_64F);
+    }
 
     auto start = std::chrono::high_resolution_clock::now();
     cv::SVD::compute(B,w,u,v);
