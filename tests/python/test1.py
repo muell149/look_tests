3ec8c7c2b3e55d91fcc51d7fb6c1a83ca79cc9f1
Uzmar Gomez Yañez <uzmar.gomez@stkprojects.com>
2020-02-06 16:11:50 -0600

    Minimal change to python code

--- tests/python/test1.py
+++ tests/python/test1.py
@@ -1,20 +1,41 @@
 import matplotlib.pyplot as plt
- 
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_lfw_people
 from sklearn.metrics import classification_report
 from sklearn.decomposition import PCA
 from sklearn.neural_network import MLPClassifier
- 
- 
+
+#********************* Test functions ************************
+def plotimagewomean(x):
+   b = x.mean(0)
+   
+   plt.figure(1)
+   plt.imshow(x-b,cmap=plt.cm.gray)
+
+   plt.figure(2)
+   plt.imshow(x,cmap=plt.cm.gray)
+
+   plt.show()
+#*************************************************************
+
+
 # Load data
 lfw_dataset = fetch_lfw_people(min_faces_per_person=100,color=False)
 
 n_samples, h, w = lfw_dataset.images.shape
 
-# plt.imshow(lfw_dataset.images[1000].reshape(h,w),cmap=plt.cm.gray)
-# plt.show()
+X = np.reshape(lfw_dataset.images,(h*w,n_samples))
+U, Sigma, D = np.linalg.svd(X, full_matrices=True)
 
+print(X.shape)
+print(U.shape)
+print(U.shape)
+print(D.shape)
 
-
-print(lfw_dataset.images[1].reshape(h*w))
+a = np.random.randn(9, 6) + 1j*np.random.randn(9, 6)
+u, s, vh = np.linalg.svd(a, full_matrices=True)
+print(a.shape)
+print(u.shape)
+print(s.shape)
+print(vh.shape)
