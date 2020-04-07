c110461d6ae815c55256116b7b0ec4bbf91a31e9
uzmargomez <uzmar.gomez@ciencias.unam.mx>
2020-04-07 12:47:33 -0500

    Change model to MTCNN

--- faceRecognition/lib/Pipeline.py
+++ faceRecognition/lib/Pipeline.py
@@ -1,28 +1,38 @@
 import cv2
+import glob
+from FaceAlign import get_aligned_face
+from FaceDetector import FaceDetectorDlib, FaceDetectorHaar
+import operator
+from functools import reduce
+from mtcnn import MTCNN
 
-from lib.FaceAlign import get_aligned_face
-from lib.FaceDetector import FaceDetectorDlib, FaceDetectorHaar
+detector = MTCNN()
 
-detector = FaceDetectorHaar()
+def detect_and_align(path, w=250, h=300, vis=False):
+    image = cv2.cvtColor(cv2.imread(path),cv2.COLOR_BGR2RGB)
 
-def detect_and_align(path, w=10, h=12, vis=False):
-    image = cv2.imread(path, 1)                 # Read as BGR image
-
-    if vis:
-        cv2.imshow("image", image)
-        key = cv2.waitKey(1)
-
-    box = detector.detect(image)
-    if len(box) is 0:
+    result = detector.detect_faces(image)
+    print(len(result))
+    if len(result) is 0:
+        print(path)
         return None                             # Return None if no face is found
     else:
-        box = box[0]
+        box = result[0]['box']
 
     crop = get_aligned_face(image, box, w, h)
     graycrop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
 
     if vis:
         cv2.imshow("crop", graycrop)
-        key = cv2.waitKey(1)
+        cv2.waitKey(1)
 
     return graycrop
+
+# images_subjects = []
+# dir = "../datasets/LookDataSet/*"
+# for directory in glob.glob(dir):
+#     images_subjects.append(glob.glob(directory+"/*.jpg"))
+# 
+# listatotal=reduce(operator.concat, images_subjects)
+# for i in listatotal:
+#     detect_and_align(path=i,vis=True)
