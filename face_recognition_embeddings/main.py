1e8f697d05d3a8dd8d1480e36b77ab4764b4c0df
uzmargomez <uzmar.gomez@ciencias.unam.mx>
2020-05-13 10:55:51 -0500

    Minor changes

--- face_recognition_embeddings/main.py
+++ face_recognition_embeddings/main.py
@@ -7,8 +7,8 @@ def main():
 	parser.add_argument("--directory",			"-dir",	help="Dataset directory",			type=str,	default="datasets/LookDataSet")
 	parser.add_argument("--extension",			"-ext",	help="Dataset images extension",	type=str,	default="jpg")
 	parser.add_argument("--size",				"-si",	help="Image size",					type=int,	default=24)
-	parser.add_argument("--scope_limit",		"-sl",	help="Scope Limit",					type=float, default=.5)
-	parser.add_argument("--intercept_limit",	"-il",	help="Intercept Limit",				type=float,	default=.17)
+	parser.add_argument("--scope_limit",		"-sl",	help="Scope Limit",					type=float, default=.6)
+	parser.add_argument("--intercept_limit",	"-il",	help="Intercept Limit",				type=float,	default=.19)
 	args = parser.parse_args()
 	
 	ds = DataSet(
@@ -20,12 +20,12 @@ def main():
     )
 	
 	ds.print_dataset_info()
-	ds.load_model(name='model',train=True)
+	ds.load_model(name='model',train=False)
 	
 	# filename='datasets/LookDataSet/Test/Emma_Watson/Emma_Watson_018_resized.jpg'
 	# ds.single_image(filename=filename)
 	
-	ds.test_model(graphs=True,print_detailed=True)
+	ds.test_model(graphs=False,print_detailed=False)
 	
 	#ds.testing_webcam()
 
